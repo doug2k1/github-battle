b4701d9e906378bae316cef97eb9eec4672cd5e1
Douglas Matoso <doug2k1@gmail.com>
2017-05-29 23:47:54 -0300

    Add router and new components.

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Nav from './Nav'
+import Home from './Home'
+import Battle from './Battle'
 import Popular from './Popular'
 
 class App extends React.Component {
   render () {
     return (
-      <div className="container">
-        <Popular/>
-      </div>
+      <Router>
+        <div className="container">
+          <Nav/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/battle" component={Battle}/>
+            <Route path="/popular" component={Popular}/>
+            <Route render={() => <p>Not found</p> }/>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
